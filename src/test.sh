fbcfcdb7b90f32245125b222043b52aced0f9e0c
Yassin Ahmed <ya4877166@gmail.com>
2026-02-27 15:12:58 +0200

    solving test problem

--- src/test.sh
+++ src/test.sh
@@ -2,7 +2,7 @@
 
 #src/test.sh
 EXPECTED_OUTPUT="Hello, World!"
-OUTPUT=$(node -e "console.log(require('./app')('World'))")
+OUTPUT=$(node -e "console.log(require('./src/app')('World'))")
 if [ "$OUTPUT" == "$EXPECTED_OUTPUT" ]; then
     echo "Test passed!"
     exit 0
